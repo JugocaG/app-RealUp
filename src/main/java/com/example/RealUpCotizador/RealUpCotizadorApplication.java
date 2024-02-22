9f98c733b3d7caaed7c02de943831959a82585fc
Juan David <juandagomezc1234@hotmail.com>
2024-02-21 23:32:40 -0500

    RealUp Cotizador Start

---
+++ src/main/java/com/example/RealUpCotizador/RealUpCotizadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealUpCotizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealUpCotizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealUpCotizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RealUpCotizador/RealUpCotizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealUpCotizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealUpCotizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
